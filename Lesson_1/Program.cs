65b4a4c3a16080831938c2b3f33c0220695924c6
Kiririn <ya.aleks19982@yandex.ru>
2021-01-22 17:30:12 +0300

    Добавьте файлы проекта.

---
+++ Lesson_1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float getDamage = 0;
+            float absorbDamade = 10;
+            double money = 100;
+            int killCount = 5;
+            int deathCount = 100;
+            string personName = "Boris";
+            string personClass = "Saber";
+            bool flyAbility = false;
+            char threatLevel = 'A';
+            bool enуmyRage = true;
+
+        }
+    }
+}
