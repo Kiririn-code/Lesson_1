1e6a72e3a802d36ceab929626c4fa49badb99b3e
Kiririn <ya.aleks19982@yandex.ru>
2021-01-22 18:29:09 +0300

    delite if-else

--- Lesson_1/Program.cs
+++ Lesson_1/Program.cs
@@ -15,17 +15,14 @@ namespace Lesson_1
             int courseCristal = 30;
             Console.WriteLine("Добрый день,сколько золотых у вас есть?");
             goldInAccount = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Вы можите купить только " + (goldInAccount/30) + " кристалов");
             Console.WriteLine("Cколько кристалов вы хотите купить?");
             cristal = Convert.ToInt32(Console.ReadLine());
-            if (goldInAccount>=cristal* courseCristal)
-            {
-                goldInAccount -= cristal * courseCristal;
-                Console.WriteLine("Вы получили " + cristal + " кристалов по курсу 30 золотых за кристал");
-                Console.WriteLine("У вас осталось " + goldInAccount + " золотых");
-            }
-            else
-                Console.WriteLine("У вас недостаточно денег,приходите в другой раз");
-            
+            goldInAccount -= cristal * courseCristal;
+            Console.WriteLine("Вы получили " + cristal + " кристалов по курсу 30 золотых за кристал");
+            Console.WriteLine("У вас осталось " + goldInAccount + " золотых");
+
+
         }
     }
 }
