742c127b366c1f6b4599b631c5e5a8ac17a0fe0f
Kiririn <ya.aleks19982@yandex.ru>
2021-01-23 14:36:53 +0300

    text count

--- Lesson_1/Program.cs
+++ Lesson_1/Program.cs
@@ -10,19 +10,18 @@ namespace Lesson_1
     {
         static void Main(string[] args)
         {
-            int cristal = 0;
-            int goldInAccount = 0;
-            int courseCristal = 30;
-            Console.WriteLine("Добрый день,сколько золотых у вас есть?");
-            goldInAccount = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Вы можите купить только " + (goldInAccount/30) + " кристалов");
-            Console.WriteLine("Cколько кристалов вы хотите купить?");
-            cristal = Convert.ToInt32(Console.ReadLine());
-            goldInAccount -= cristal * courseCristal;
-            Console.WriteLine("Вы получили " + cristal + " кристалов по курсу 30 золотых за кристал");
-            Console.WriteLine("У вас осталось " + goldInAccount + " золотых");
+            int textCount;
+            string textMassage;
 
+            Console.WriteLine("Добрый день,Введите ваше сообщение");
+            textMassage = Console.ReadLine();
+            Console.WriteLine("Введите количество повторений");
+            textCount = Convert.ToInt32(Console.ReadLine());
 
+            for (int i = 0; i < textCount; i++)
+            {
+                Console.WriteLine(textMassage);
+            }
         }
     }
 }
