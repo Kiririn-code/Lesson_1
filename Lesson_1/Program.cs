88d8cbe7286b1ea7f6c87adf9f756bd8142b683c
Kiririn <ya.aleks19982@yandex.ru>
2021-01-26 15:19:34 +0300

    passwd

--- Lesson_1/Program.cs
+++ Lesson_1/Program.cs
@@ -10,11 +10,22 @@ namespace Lesson_1
     {
         static void Main(string[] args)
         {
-            string userInput = "";
-            while (userInput != "exit")
+            string userPassword = "";
+            string correctPassword = "флекс";
+            for (int i = 3; i >0; i--)
             {
-                Console.Write("Введите коловое слово: ");
-                userInput = Console.ReadLine();
+                Console.WriteLine("Введи параоль,челик");
+                userPassword = Console.ReadLine();
+                if(userPassword == correctPassword)
+                {
+                    Console.WriteLine("Молодец,челик,вот ты флексишь");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("УуУу,пароль не верный,ну ты кринж");
+                    Console.WriteLine("У тебя осталось " + (i-1)+ " попыток");
+                }
             }
         }
     }
