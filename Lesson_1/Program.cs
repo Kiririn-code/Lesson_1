ed0447ce3956da1254cc5a93c5654790fc46a045
Kiririn <ya.aleks19982@yandex.ru>
2021-01-30 15:36:18 +0300

    -

--- Lesson_1/Program.cs
+++ Lesson_1/Program.cs
@@ -10,29 +10,10 @@ namespace Lesson_1
     {
         static void Main(string[] args)
         {
-            int[] array = new int[30];
-           Random random = new Random();
+            string userAction;
+            while (true)
+            {
 
-            for (int i = 0; i < array.Length; i++)
-            {
-                array[i] = random.Next(50);
-                Console.WriteLine($"элемент - {(i+1)} Число - {array[i]}");
-            }
-
-            for (int i = 1; i < array.Length-1; i++)
-            {
-                if (array[i] > array[i - 1] && array[i] > array[i + 1])
-                {
-                    Console.WriteLine($"Локальный максимум обнаружен под номером {i+1}");
-                }
-            }
-            if (array[0] > array[1])
-            {
-                Console.WriteLine("Локальный максимум обнаружен под номером 1");
-            }
-            if (array[array.Length-1] > array[array.Length-2])
-            {
-                Console.WriteLine($"Локальный максимум обнаружен под номером {array.Length}");
             }
         }
     }
