7fc6d7fc7796de2c82393f3b866740d7b6e8c12d
Kiririn <ya.aleks19982@yandex.ru>
2021-01-31 12:16:42 +0300

    Fix

--- Lesson_1/Program.cs
+++ Lesson_1/Program.cs
@@ -38,26 +38,23 @@ namespace Lesson_1
                         Console.ReadKey();
                         Console.Clear();
                         break;
-                    case "1":
-                    case "2":
-                    case "3":
-                    case "4":
-                    case "5":
-                    case "6":
-                    case "7":
-                    case "8":
-                    case "9":
-                    case "0":
-                        int[] expandArray = new int[array.Length + 1];
-                        for (int i = 0; i < array.Length; i++)
-                        {
-                            expandArray[i] = array[i];
-                        }
-                        expandArray[expandArray.Length - 1] = Convert.ToInt32(userAction);
-                        array = expandArray;
-                        break;
+
                     default:
-                        Console.WriteLine("Я не знаю,что вы написали");
+                        try
+                        {
+                            int elementOfArray = Convert.ToInt32(userAction);
+                            int[] expandArray = new int[array.Length + 1];
+                            for (int i = 0; i < array.Length; i++)
+                            {
+                                expandArray[i] = array[i];
+                            }
+                            expandArray[expandArray.Length - 1] =elementOfArray ;
+                            array = expandArray;
+                        }
+                        catch(Exception)
+                        {
+                            Console.WriteLine("Введены не корректные данные");
+                        }   
                         break;
                 }
             }
