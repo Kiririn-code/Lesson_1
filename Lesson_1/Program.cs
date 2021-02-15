e691e5182a31680982ec166565d20c67f3ed232f
Kiririn <ya.aleks19982@yandex.ru>
2021-02-15 11:04:07 +0300

    player

--- Lesson_1/Program.cs
+++ Lesson_1/Program.cs
@@ -10,28 +10,27 @@ namespace Lesson_1
     {
         static void Main(string[] args)
         {
-            int sum = 0;
-            Queue<int> purchase = new Queue<int>();
-
-            purchase.Enqueue(0);
-            purchase.Enqueue(4);
-            purchase.Enqueue(2);
-            purchase.Enqueue(5);
-            purchase.Enqueue(7);
-            purchase.Enqueue(9);
-            purchase.Enqueue(100);
-            purchase.Enqueue(150);
-            purchase.Enqueue(200);
-
-
-            while(purchase.Count>0)
-            { 
-                sum += purchase.Dequeue();
-                Console.WriteLine(sum);
-                Console.ReadKey();
-                Console.Clear();
-            }
-                    
+            Player player = new Player("эльф", "Аристарх", 22);
+            player.ShowInfo();
         }
     }
+    public class Player 
+    {
+        private string _race;
+        public string Name;
+        public int Age;
+
+        public Player(string race, string name, int age)
+        {
+            _race = race;
+            Name = name;
+            Age = age;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Вас зовоут {Name} Вы {_race} и Вам {Age} лет");
+        }
+    }
+
 }
