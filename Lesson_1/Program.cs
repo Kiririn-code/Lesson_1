b16faea1745f6ecd53b45df2005acad828fb75d6
Kiririn <ya.aleks19982@yandex.ru>
2021-03-04 18:50:02 +0300

    draft

--- Lesson_1/Program.cs
+++ Lesson_1/Program.cs
@@ -10,32 +10,7 @@ namespace Lesson_1
     {
         static void Main(string[] args)
         {
-            Player player = new Player(5, 1);
-            Drawer drawer = new Drawer();
-            drawer.DrawPlayer(player);
-        }
-    }
-    class Player
-    {
-        public int X { get; private set; }
-        public int Y { get; private set; }
-        public Player(int x, int y)
-        {
-            X = x;
-            Y = y;
-        }
-    }
 
-    class Drawer
-    {
-
-        public void DrawPlayer(Player player)
-        { 
-            Console.SetCursorPosition(player.X,player.Y);
-            Console.WriteLine("@");
-            Console.CursorVisible = false;
-            Console.ReadKey(true);
-            
         }
     }
 }
