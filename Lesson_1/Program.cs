60adfbb5ca259689b285b0c5bcf126b191e663f8
Kiririn <ya.aleks19982@yandex.ru>
2021-03-11 11:30:54 +0300

    Tabula Rasa

--- Lesson_1/Program.cs
+++ Lesson_1/Program.cs
@@ -8,56 +8,7 @@ namespace Lesson_1
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            DataBase dataBase = new DataBase();
 
-            dataBase.TransportSoliders();
-        }
     }
 
-    class DataBase
-    {
-        private List<Solider> _firstPlatoon = new List<Solider>();
-        private List<Solider> _secondPlatoon = new List<Solider>();
-
-        public DataBase()
-        {
-            _firstPlatoon.Add(new Solider("Петров"));
-            _firstPlatoon.Add(new Solider("Баширов"));
-            _firstPlatoon.Add(new Solider("Кузьмин"));
-            _firstPlatoon.Add(new Solider("Баранов"));
-            _firstPlatoon.Add(new Solider("Бойко"));
-            _firstPlatoon.Add(new Solider("Пугачев"));
-            _firstPlatoon.Add(new Solider("Березуцкий"));
-            _firstPlatoon.Add(new Solider("Абрамов"));
-            _firstPlatoon.Add(new Solider("Яйцев"));
-
-            _secondPlatoon.Add(new Solider("Авдаченок"));
-            _secondPlatoon.Add(new Solider("Лебедев"));
-            _secondPlatoon.Add(new Solider("Максимоа"));
-            _secondPlatoon.Add(new Solider("Пасечник"));
-        }
-
-        public void TransportSoliders()
-        {
-            _secondPlatoon = _secondPlatoon.Union(_firstPlatoon.Where(solider => solider.Surname.StartsWith("Б"))).ToList();
-            _firstPlatoon = _firstPlatoon.Except(_firstPlatoon.Where(solider => solider.Surname.StartsWith("Б"))).ToList();
-
-            foreach (var solider in _secondPlatoon)
-            {
-                Console.WriteLine(solider.Surname);
-            }
-        }
-    }
-
-    class Solider
-    {
-        public string Surname { get;private set; }
-
-        public Solider(string surname)
-        {
-            Surname = surname;
-        }
-    }
 }
