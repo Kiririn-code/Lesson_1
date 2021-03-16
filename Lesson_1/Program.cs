458a6542c093aef2a7452e8248153f3cb3941486
Kiririn <ya.aleks19982@yandex.ru>
2021-03-16 11:54:38 +0300

    Fix

--- Lesson_1/Program.cs
+++ Lesson_1/Program.cs
@@ -19,13 +19,14 @@ namespace Lesson_1
     {
         private Queue<Person> _people = new Queue<Person>();
         private Random _random = new Random();
-        public int Money { get;private set; }
 
         public Market()
         {
+            _random = new Random();
+
             for (int i = 0; i < 6; i++)
             {
-                _people.Enqueue(new Person(_random.Next(400,900)));
+                _people.Enqueue(new Person(_random.Next(500,1000)));
             }
         }
 
@@ -35,38 +36,38 @@ namespace Lesson_1
             {
                 Person people = _people.Dequeue();
                 Console.WriteLine($"Добрый день, посетитель, удачных покупок");
-                loop:
-                    if (people.Money > people.GetCoast())
-                    {
-                        Console.WriteLine("Покупки успешно оплачены\nВаш список покупок:");
-                        people.ShowList();
-                    }
-                    else
-                    {
+
+                while (people.Money<people.GetCoast())
+                {
                         Console.WriteLine($"У вас всего { people.Money} рублей,а продуктовая корзина стоит {people.GetCoast()} рублей");
                         Console.WriteLine("Вам не зватает денег, придется выбросить что то");
                         people.DeleteProduct();
-                        goto loop;
-                    }
+                }
+                Console.WriteLine("Покупки успешно оплачены\nВаш список покупок:");
+                people.ShowList();
                 Console.ReadKey();
                 Console.Clear();
             }
         }
     }
+
    class Person
     {
+        private List<Product> _products;
+        private static Random _random;
+
         public int Money { get;private set; }
-        private List<Product> _products = new List<Product>();
-        private static Random _random = new Random();
 
         public Person(int money)
         {
             Money = money;
+            _random = new Random();
+            _products = new List<Product>();
 
             for (int i = 0; i < _random.Next(5,11); i++)
             {
                 int id = _random.Next(7);
-                _products.Add(new Product(Product.BaseOfCoast(id), Product.BaseOfName(id)));
+                _products.Add(new Product(id));
             }
         }
 
@@ -99,20 +100,12 @@ namespace Lesson_1
         public int Coast { get; private set; }
         public string Name { get; private set; }
 
-        public Product(int coast, string name)
+        public Product(int id)
         {
-            Coast = coast;
-            Name = name;
-        }
-       static public string BaseOfName(int id)
-        {
-            string[] typeOfProducts = { "Колбаса", "Макароны", "Сыр", "Водка", "Печенье","Сосиски","Кетчуп"};
-            return typeOfProducts[id];
-        }
-        static public int BaseOfCoast(int id)
-        {
-            int[] coastofProducts = { 150, 66, 189, 370, 55, 78, 36 };
-            return coastofProducts[id];
+            string[] typeOfProducts = { "Колбаса", "Макароны", "Сыр", "Водка", "Печенье", "Сосиски", "Кетчуп" };
+            int[] coastOfProducts = { 150, 66, 189, 370, 55, 78, 36 };
+            Coast = coastOfProducts[id];
+            Name = typeOfProducts[id];
         }
     }
 }
